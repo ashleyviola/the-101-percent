b1ccd45392d5d89bc56f4c6448a45a85ada72fc2
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-11-13 11:20:25 -0500

    updated saved search functionality and create dynamic button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,83 @@
 let stockSearchEl = document.querySelector("#stock-search-form");
 let stockNameInputEl = document.querySelector("#stock-call-sign");
+let searchHistoryEl = document.querySelector("#search-history");
 let modalEl = document.querySelector(".modal");
 let modalExitBtn = document.querySelector("close");
-
+let recentSearchCounter = [];
+let recentSearches = [];
 
 // Search Input Handler 
 let formSubmitHandler = function(event){
     event.preventDefault();
-
-    let stockCallName = stockNameInputEl.value.trim();
-    if (stockCallName){
-        // add condition to have it check stvio for stock ticker to make sure that it's valid 
-        if(stockCallName.length < 6){
-            console.log(stockCallName);
-            // enter fetch functions here 
-        } else {
-            displayModalHandler();
-        }
-    } else { 
+    let stockCallNameInput = stockNameInputEl.value.trim();
+   
+    // check if inputs are empty
+    if(!stockCallNameInput){
         displayModalHandler();
+        return false;
     }
+    // NEED TO ADD VALIDATION OF TICKER VALUE FROM API 
+
+    stockNameInputEl.value = ""
+
+    // check if this is a new search or previous 
+
+    var pastSearch = stockSearchEl.hasAttribute("search-id");
+    if (pastSearch){
+        // var searchId = stockSearchEl.getAttribute("search-id");
+        // console.log("this has been previously searched");
+    } else {
+        var recentSearchObj = {
+            ticker: stockCallNameInput 
+        };
+        console.log(recentSearchObj)
+        createRecentSearchBtns(recentSearchObj)
+    }
+
 };
 
+let createRecentSearchBtns = function(recentSearchObj){
+    // creating button element 
+    var savedListItemEl = document.createElement("li");
+    savedListItemEl.className = "saved-item";
+    savedListItemEl.setAttribute("search-id", recentSearchCounter);
+
+    var savedTickerBtnEl = document.createElement("button");
+    savedTickerBtnEl.className = "saved-ticker-btn";
+    savedTickerBtnEl.innerHTML = "<p class='ticker-name'>" + recentSearchObj.ticker + "</p>";
+
+    savedListItemEl.appendChild(savedTickerBtnEl);
+    searchHistoryEl.appendChild(savedListItemEl);
+    // save task as an object with name and id 
+    recentSearchObj.id = recentSearchCounter;
+
+    recentSearches.push(recentSearchObj);
+
+    // save tasks to localStorage
+    saveSearchInput();
+    
+    // increase task counter for next task id 
+    recentSearchCounter++;
+}
+
+// save search input to localstorage 
+let saveSearchInput = function(){
+    localStorage.setItem("stockTickers", JSON.stringify(recentSearches));
+}
+
+let loadSearches = function(){
+    let savedSearches = localStorage.getItem("stockTickers");
+    if(!savedSearches){
+        return false;
+    }
+    console.log("saved tasks found!")
+    
+    savedSearches = JSON.parse(savedSearches);
+    console.log(savedSearches);
+//     for (let i = 0; i < savedSearches.length; i++){
+//         createRecentSearchBtns(savedSearches[i]);
+//     }
+}
 
 // modal functions 
 let displayModalHandler = function(){
@@ -31,7 +87,8 @@ let exitModalHandler = function(){
     modalEl.style.display = "none";
 };
 
-
 // Event Listeners 
 stockSearchEl.addEventListener("submit",formSubmitHandler);
 modalEl.addEventListener("click",exitModalHandler);
+
+loadSearches();
