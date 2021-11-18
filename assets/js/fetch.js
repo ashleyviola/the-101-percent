58c0662580179e3198e534e0d26531a69f281706
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-11-18 04:38:25 -0600

    fixed retrieve reddit function

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,5 +1,6 @@
 let commentArr = [];
 
+
 // fetch the wallstreet bets api
 var token
 var userName = "upojT-rxULWxHaohV2favg"; // app client ID
@@ -22,15 +23,20 @@ function getToken(url, clientID, secret) {
             key = obj.access_token; // declare and pull key from response
             token = key;
             // console.log(token);
+            redditRetrieve(token);
             return token;
 
         }
+
         else {
-            console.log("Error");
+            console.log("Network Error");
         }
-    })
-});
+    });
 }
+// the standard fetch function for retrieve data from the wsb subreddit
+let redditRetrieve = function(token) {
+    let postIds = [];
+
     if (token) {
         let url = 'https://oauth.reddit.com/r/wallstreetbets/hot'; // url variable used to fetch wsb subreddit
         let data ={
@@ -49,27 +55,26 @@ function getToken(url, clientID, secret) {
             .then(function (response) {
                 if (response.ok) {
                     response.json().then(function(data)
-                    { // cycle through post data received from reddit to obtain the post ids of the posts in HOT
+                    {
+                        // console.log(data.data.children[0].data.id)
 
                         // for loop that finds the post id for posts in 'hot' on the wallstreetbets subreddit
                         for (let i = 0; i < data.data.children.length; i++) {
                             let postUrl = data.data.children[i].data.id
                             postIds.push(postUrl);
                             if (postIds.length > 10) {
-                                // stop after 10 post IDs
                                 return postIds;
                             }
+                            // console.log(postIds);
 
-                            // cycle through the post IDs creating a new fetch url for each post
                             for (let x = 0; x < postIds.length; x++) {
                                 let newUrl = 'https://oauth.reddit.com/r/wallstreetbets/comments/' + postIds[x];
                                 if (newUrl) {
-                                    // fetch the post urls and cycle through to find the comments in each post
                                     fetch(newUrl, otherPram).then(function (response) {
                                         response.json().then(function(data) {
+                                            // console.log(data[1].data.children[1].data.body);
                                             for (let i = 0; i < data[1].data.children.length; i++) {
                                                 let postComments = data[1].data.children[i].data.body;
-                                                // send the postComments to the storeData() function
 
                                                 storeData(postComments);
                                             }
@@ -82,12 +87,8 @@ function getToken(url, clientID, secret) {
                     })
                 }
             })
-        }
 
-var displayStocks = function() {
-    if (response[i].symbol = "APPL");
-    var comments = response[i].matchScore;
-    console.log(data);
+    }
 }
 // the store function for pulling comment and post data from the wsb subreddit
 let storeData = function(data) {
@@ -101,11 +102,9 @@ let storeData = function(data) {
     if (commentData.length <= 1000) {
 
             for (let i = 0; i < hotArr.length; i++) {
-                // push each comment into the commentData array
                 commentData.push(hotArr[i]);
 
                 if (commentData.length >= 1000) {
-                    // if the fetch pulls over 1000 comments stop adding new comments and call the sortData() function
                     sortData(commentData);
                     break;
                     }
@@ -116,34 +115,30 @@ let storeData = function(data) {
 
 let sortData = function(comments) {
     let commentData = commentArr;
-    // create new empty variables and arrays for userInput and Data splitting
     let result;
     let recentSearch = [];
     let splitData = [];
-    // retrieve recently searched tickers from local storage
     let savedSearches = localStorage.getItem("stockTickers");
     savedSearches = JSON.parse(savedSearches);
 
     for (i = 0; i < savedSearches.length; i++)
     {
         recentSearch.push(savedSearches[i].ticker);
-        // push tickers from local storage to new array
     for (j = 0; j < comments.length; j++)
     {   
         if (!recentSearch[i] || !comments[j]) {
-            // if the fetch failed to retrieve comments try again
             redditRetrieve(token);
             return;
         }
-        // check to see if any of the comments from reddit contain the ticker the user is looking for
         comments[j].includes(recentSearch[i]) ? (splitData.push(comments[j])): "";
         let result = splitData
-        // if they do then call the getSentiment() function with the result (array of comments containing ticker data)
+
         if (comments[j].includes(recentSearch[i])) {
-            getSentiment(result);
+            console.log(splitData);
+            getSentiment(splitData);
         }
         else if (splitData.length >= 10) {
-            return result;
+            return splitData;
         }
                 
     }
@@ -152,39 +147,21 @@ let sortData = function(comments) {
 }
 
 let getSentiment = function(data) {
-    // create an empty string that will become sentiment value
     let getSen = "";
 
     for (i = 0; i < data.length; i++) {
-        // create variable called indStr for each individual string data holds
         let indStr = data[i];
-        // check if the strings contain keywords that would infer the response is positive
         if (indStr.includes("buy" || "huge" || "moon" || "big" || "green" || "returns" || "bullish" || "bulls" || "🚀")) {
             getSen = "BUY";
         }
         else {
-        // if they do not contain those keywords the sentiment is negative
             getSen = "SELL";
         }
         console.log(getSen);
-        // call the createWsbSentiment in script.js with this data
         createWsbSentiment(getSen);
         return getSen;
     }
 
-};
-
-// Sort Data Here
-
-//check api returns
-var getTickerData = function(ticker) {
-    if (ticker.length === 0) {
-        symbol.textContent = "No ticker found.";
-        return;
-
-    }
 }
 
-//for (var i = 0; i < ticker.length; i++) {
-    
-//
+getToken(tokenUrl, userName, password);
